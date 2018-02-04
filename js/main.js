315aaa9f5064ff914c9f8e020b38f95851648563
Andrey Kustov <kustovforwork@gmail.com>
2018-02-04 21:32:17 +0300

    third homework(4)

--- js/main.js
+++ js/main.js
@@ -6,3 +6,34 @@ compactMenu.addEventListener("click", function(event) {
     about.classList.toggle("open");
     document.body.classList.toggle("no-scroll");
 });
+
+
+const detailsClose = document.querySelector(".details__close-wrap");
+const details = document.querySelector(".details");
+const components = document.querySelector(".burgers__components-icon");
+
+components.addEventListener("click", function() {
+    if (parseInt(window.innerWidth) <= 768) {
+        components.style.opacity = "1";
+        details.style.opacity = "1";
+        details.style.left = "100%";
+    }
+});
+
+detailsClose.addEventListener("click", function() {
+    components.style.opacity = "0.4";
+    details.style.opacity = "0";
+    details.style.left = "-9999px";
+});
+
+const menuPoints = document.querySelectorAll(".about__item");
+const menuPointsNav = document.querySelectorAll(".nav__item-link");
+
+for (let i = 0; i < menuPoints.length; i++) {
+    menuPoints[i].addEventListener("click", function(event) {
+        event.preventDefault();
+    });
+    menuPointsNav[i].addEventListener("click", function(event) {
+        event.preventDefault();
+    });
+}
